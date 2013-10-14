fb3a494fec14334c6dea318fa9b46ababf4d0218
Michael Herman <hermanmu@gmail.com>
2013-10-13 20:19:22 -0600

    initial

---
+++ app/app.js
@@ -0,0 +1,29 @@
+var weather = angular.module('weather', []);
+weather.controller('WeatherCtrl', function($scope, $http) {
+	$scope.data = [{}];
+	$scope.woeid = 0;
+	$scope.query = '';
+	$scope.loadWeather = function() {
+		if ($scope.query !== '' && /^\d{5}(-\d{4})?$/.test($scope.query)) {
+			$http.jsonp('http://query.yahooapis.com/v1/public/yql?', {
+				params: {
+					q: 'select * from geo.places where text="' + $scope.query + '"',
+					format: 'json',
+					callback: 'JSON_CALLBACK'
+				}
+			}).success(function(data) {
+				$scope.woeid = data.query.results.place[0].woeid;
+			}).then(function() {
+				$http.jsonp('http://query.yahooapis.com/v1/public/yql?', {
+					params: {
+						q: 'select * from weather.forecast where woeid=' + $scope.woeid + '',
+						format: 'json',
+						callback: 'JSON_CALLBACK'
+					}
+				}).success(function(data) {
+					$scope.data = data.query.results.channel;
+				});
+			});
+		}
+	};
+});
